8c33305a34703ec7d6f2248186ff5ac3e24a2447
luisfelipeluis49 <luisfelipeluis49@gmail.com>
2020-07-11 17:27:01 -0300

    created

---
+++ src/test/java/com/webhostapp/cafeliteratureclub/CafeliteratureclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webhostapp.cafeliteratureclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeliteratureclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
